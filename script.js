18a3848cdea9b40fb2f77ee64eb14c6758266115
JereRoa <jereroa.digital@gmail.com>
2023-08-22 18:07:05 -0400

    se-agregó-un-tercer-color-al-grafico

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const rgbRedColor = 'rgb(255, 99, 132)'
 const rgbaOrangeColor = 'rgba(255, 159, 64, 0.2)'
 const rgbOrangeColor = 'rgb(255, 159, 64)'
 
+const rgbaBlueColor = 'rgba(54, 162, 235, 0.2)'
+const rgbBlueColor = 'rgb(54, 162, 235)'
 
 async function renderData() {
     const weathers = await fetchApi('https://api.gael.cloud/general/public/clima')
@@ -18,8 +20,27 @@ async function renderData() {
     console.log(temperatures)
     console.log(stations)
 
-    const backgroundColors = temperatures.map(temperatures => temperatures <20 ? rgbaOrangeColor : rgbaRedColor)
-    const borderColors = temperatures.map(temperatures => temperatures <20 ? rgbOrangeColor : rgbRedColor)
+    const backgroundColors = temperatures.map(temperatures => {
+        if (temperatures <= 5) {
+          return rgbaBlueColor; // Color azul para temperaturas menores o iguales a 5
+        } else if (temperatures <= 19) {
+          return rgbaOrangeColor; // Color naranja para temperaturas entre 6 y 19
+        } else {
+          return rgbaRedColor; // Color rojo para temperaturas iguales o mayores a 20
+        }
+      });
+      
+      const borderColors = temperatures.map(temperatures => {
+        if (temperatures <= 5) {
+          return rgbBlueColor; // Color azul para temperaturas menores o iguales a 5
+        } else if (temperatures <= 19) {
+          return rgbOrangeColor; // Color naranja para temperaturas entre 6 y 19
+        } else {
+          return rgbRedColor; // Color rojo para temperaturas iguales o mayores a 20
+        }
+      });
+      
+
     const ctx = document.getElementById('myChart');
 
     new Chart(ctx, {
